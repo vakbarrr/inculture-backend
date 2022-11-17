a37d68aa031ef0c17fda2c72f29434c3356058de
Gian Akbar <akbarigi20@gmail.com>
2022-11-17 12:55:54 +0700

    Update menu.blade.php

--- resources/views/panel/layouts/menu.blade.php
+++ resources/views/panel/layouts/menu.blade.php
@@ -35,15 +35,15 @@
             <li class="kt-menu__item {{ CMS::set_active('berita.index') }} {{ CMS::set_active('berita.edit') }}"
                 aria-haspopup="true"><a href="{{ route('berita.index') }}" class="kt-menu__link "><i
                         class="kt-menu__link-bullet kt-menu__link-bullet--dot"><span></span></i><span
-                        class="kt-menu__link-text">Semua Blog</span></a></li>
+                        class="kt-menu__link-text">All Blog</span></a></li>
             <li class="kt-menu__item {{ CMS::set_active('berita.create') }}" aria-haspopup="true"><a
                     href="{{ route('berita.create') }}" class="kt-menu__link "><i
                         class="kt-menu__link-bullet kt-menu__link-bullet--dot"><span></span></i><span
-                        class="kt-menu__link-text">Tambah Baru</span></a></li>
+                        class="kt-menu__link-text">Add New</span></a></li>
             <li class="kt-menu__item {{ CMS::setActive('kategori') }}" aria-haspopup="true"><a
                     href="{{ route('kategori.index') }}" class="kt-menu__link "><i
                         class="kt-menu__link-bullet kt-menu__link-bullet--dot"><span></span></i><span
-                        class="kt-menu__link-text">Kategori</span></a></li>
+                        class="kt-menu__link-text">Category</span></a></li>
             <li class="kt-menu__item {{ CMS::setActive('tag') }}" aria-haspopup="true"><a
                     href="{{ route('tag.index') }}" class="kt-menu__link "><i
                         class="kt-menu__link-bullet kt-menu__link-bullet--dot"><span></span></i><span
@@ -83,6 +83,23 @@
     </div>
 </li>
 
+
+<li class="kt-menu__item  {{ CMS::set_active('pesan') }} " aria-haspopup="true"><a href="{{ route('pesan.index') }}"
+    class="kt-menu__link "><span class="kt-menu__link-icon"><svg xmlns="http://www.w3.org/2000/svg"
+            xmlns:xlink="http://www.w3.org/1999/xlink" width="24px" height="24px" viewBox="0 0 24 24" version="1.1"
+            class="kt-svg-icon">
+            <g stroke="none" stroke-width="1" fill="none" fill-rule="evenodd">
+                <polygon id="Bound" points="0 0 24 0 24 24 0 24" />
+                <path
+                    d="M12.9336061,16.072447 L19.36,10.9564761 L19.5181585,10.8312381 C20.1676248,10.3169571 20.2772143,9.3735535 19.7629333,8.72408713 C19.6917232,8.63415859 19.6104327,8.55269514 19.5206557,8.48129411 L12.9336854,3.24257445 C12.3871201,2.80788259 11.6128799,2.80788259 11.0663146,3.24257445 L4.47482784,8.48488609 C3.82645598,9.00054628 3.71887192,9.94418071 4.23453211,10.5925526 C4.30500305,10.6811601 4.38527899,10.7615046 4.47382636,10.8320511 L4.63,10.9564761 L11.0659024,16.0730648 C11.6126744,16.5077525 12.3871218,16.5074963 12.9336061,16.072447 Z"
+                    id="Shape" fill="#000000" fill-rule="nonzero" />
+                <path
+                    d="M11.0563554,18.6706981 L5.33593024,14.122919 C4.94553994,13.8125559 4.37746707,13.8774308 4.06710397,14.2678211 C4.06471678,14.2708238 4.06234874,14.2738418 4.06,14.2768747 L4.06,14.2768747 C3.75257288,14.6738539 3.82516916,15.244888 4.22214834,15.5523151 C4.22358765,15.5534297 4.2250303,15.55454 4.22647627,15.555646 L11.0872776,20.8031356 C11.6250734,21.2144692 12.371757,21.2145375 12.909628,20.8033023 L19.7677785,15.559828 C20.1693192,15.2528257 20.2459576,14.6784381 19.9389553,14.2768974 C19.9376429,14.2751809 19.9363245,14.2734691 19.935,14.2717619 L19.935,14.2717619 C19.6266937,13.8743807 19.0546209,13.8021712 18.6572397,14.1104775 C18.654352,14.112718 18.6514778,14.1149757 18.6486172,14.1172508 L12.9235044,18.6705218 C12.377022,19.1051477 11.6029199,19.1052208 11.0563554,18.6706981 Z"
+                    id="Path" fill="#000000" opacity="0.3" />
+            </g>
+        </svg></span><span class="kt-menu__link-text">Message</span></a>
+</li>
+
 <li class="kt-menu__item  kt-menu__item--submenu {{ CMS::set_open('users') }}" aria-haspopup="true"
     data-ktmenu-submenu-toggle="hover"><a href="javascript:;" class="kt-menu__link kt-menu__toggle"><span
             class="kt-menu__link-icon">
@@ -97,14 +114,14 @@
                         d="M3.00065168,20.1992055 C3.38825852,15.4265159 7.26191235,13 11.9833413,13 C16.7712164,13 20.7048837,15.2931929 20.9979143,20.2 C21.0095879,20.3954741 20.9979143,21 20.2466999,21 C16.541124,21 11.0347247,21 3.72750223,21 C3.47671215,21 2.97953825,20.45918 3.00065168,20.1992055 Z"
                         id="Mask-Copy" fill="#000000" fill-rule="nonzero" />
                 </g>
-            </svg></span><span class="kt-menu__link-text">Pengguna</span><i
+            </svg></span><span class="kt-menu__link-text">Management Users</span><i
             class="kt-menu__ver-arrow la la-angle-right"></i></a>
     <div class="kt-menu__submenu "><span class="kt-menu__arrow"></span>
         <ul class="kt-menu__subnav">
             <li class="kt-menu__item {{ CMS::set_active('users.index') }}  {{ CMS::set_active('users.edit') }} "
                 aria-haspopup="true"><a href="{{ route('users.index') }}" class="kt-menu__link "><i
                         class="kt-menu__link-bullet kt-menu__link-bullet--dot"><span></span></i><span
-                        class="kt-menu__link-text">Semua Pengguna</span></a></li>
+                        class="kt-menu__link-text">All user</span></a></li>
         </ul>
     </div>
 </li>
@@ -123,18 +140,14 @@
                         d="M12,15 C13.6568542,15 15,13.6568542 15,12 C15,10.3431458 13.6568542,9 12,9 C10.3431458,9 9,10.3431458 9,12 C9,13.6568542 10.3431458,15 12,15 Z"
                         fill="#000000" />
                 </g>
-            </svg></span><span class="kt-menu__link-text">Pengaturan</span><i
+            </svg></span><span class="kt-menu__link-text">Setting</span><i
             class="kt-menu__ver-arrow la la-angle-right"></i></a>
     <div class="kt-menu__submenu "><span class="kt-menu__arrow"></span>
         <ul class="kt-menu__subnav">
             <li class="kt-menu__item {{ CMS::setActive('setting') }}" aria-haspopup="true">
                 <a href="{!! route('setting.index') !!}" class="kt-menu__link "><i
                         class="kt-menu__link-bullet kt-menu__link-bullet--dot"><span></span></i><span
-                        class="kt-menu__link-text">Umum</span></a></li>
-            {{-- <li class="kt-menu__item {{ CMS::setActive('menu') }}" aria-haspopup="true"><a
-                    href="{{ route('menu.index') }}" class="kt-menu__link "><i
-                        class="kt-menu__link-bullet kt-menu__link-bullet--dot"><span></span></i><span
-                        class="kt-menu__link-text">Menu</span></a></li> --}}
+                        class="kt-menu__link-text">Genral setting</span></a></li>
         </ul>
     </div>
 </li>
